a738a3eed6be944d62e2cf37345cac4db9323e75
Luizhenriqueup <luiz.up.work@gmail.com>
2025-08-06 20:51:02 -0300

    Add: aula3 sala

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-// recebe 2 numeros inteiros soma e da a média no final //
 int main()
 {
-    int num;
-    int num1;
-    int soma;
-    float media;
-    printf("Informe um numero:\n");
-    scanf("%d", &num);
-    printf("informe outro numero\n");
-    scanf("%d", &num1);
-    soma = num + num1;
-    printf("Soma dos numeros: %d\n", soma);
-    media= (soma) /2.0;
-    printf ("media dos numeros informados %.2f\n", media);
+    char v1, v2, v3;
+    printf("informe o primeiro caracter:");
+    scanf("%c", &v1);
+    printf("informe o segundo caracter:");
+    scanf(" %c", &v2);
+    printf("informe o terceiro caracter:");
+    scanf(" %c",&v3);
+    printf("caracteres informados: %c %c %c\n", v1, v2, v3);
 
     return 0;
 }
