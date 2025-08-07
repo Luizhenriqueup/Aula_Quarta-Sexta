ca7f564378ccc2cb8d57316ebef6dd361c96fb22
Luizhenriqueup <luiz.up.work@gmail.com>
2025-08-06 21:39:02 -0300

    ADD: aula3 sala

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int a;
-    int b;
-    int c;
-    int x;
-    printf("informe um numero para letra a:");
-    scanf("%d", &a);
-    printf("informe um numero para letra b:");
-    scanf("%d", &b);
-    printf("informe um numero para letra c:");
-    scanf("%d", &c);
-    x = 2 * ((a - c)/8) - 5 * b;
-    printf("valor de x: %d\n", x);
+ int num;
+ int num1;
+ double soma;
+ double multiplicacao;
+ double divisao;
+ double resto;
+ printf("informe um numero");
+ scanf("%d", &num);
+ printf("informe outro numero");
+ scanf("%d", &num1);
+ soma = num + num1;
+ multiplicacao = num - num1;
+ divisao = num / num1;
+ resto = num % num1;
+ printf("soma: %lf\n", soma);
+ printf("multiplicacao: %lf\n", multiplicacao);
+ printf("divisao: %lf\n", divisao);
+ printf("resto: %lf\n", resto);
+
+
 
 
 
