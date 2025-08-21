a2f7870bcf72386f09cc6b27fde47e57a9f469a9
Luizhenriqueup <luiz.up.work@gmail.com>
2025-08-20 21:44:14 -0300

    Aula_2008

---
+++ aula2008/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    /*int num, numero;
+    printf("informe um numero:");
+    scanf("%d", &num);
+    numero = num % 2;
+    if (numero == 0){
+        printf("eh par %d", num);
+    }
+    if (numero != 0){
+        printf("eh impar %d", num);*/
+
+
+
+
+        /*int n;
+        printf("informe um numero:\n");
+        scanf("%d", &n);
+        if(n > 0){
+            printf("o numero e positivo!");
+        }
+        else{
+            printf("o numero e negativo!");*/
+
+
+            int n1, n2;
+            printf("informe um numero inteiro:");
+            scanf("%d", &n1);
+            printf("informe outro numero inteiro:");
+            scanf("%d", &n2);
+            if (n1 == n2){
+                printf("os numeros são iguais");
+            }
+            else{
+                printf("o nuemero não são iguasi");
+            }
+
+
+
+
+
+
+   return 0;
+}
