4055fe8c9fe59199136ef76fe197e949122fb5c8
Luizhenriqueup <luiz.up.work@gmail.com>
2025-09-03 21:37:25 -0300

    main.c

---
+++ aulta_0309/main.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+
+int main(){
+    /*int num, cont = 1;
+    printf("informe um numero:");
+    scanf("%d", &num);
+    if (num > 0){
+        while (cont <= num){
+            printf("%d\t",cont);
+            cont ++;
+        }
+    }
+    else{
+        printf("o numero deve ser positivo");
+    }*/
+
+
+    /*int num;
+    printf("informe um numero:");
+    scanf("%d", &num);
+    if (num > 0){
+        while (num >= 1){
+        printf("%d\t", num);
+        num--;
+        }
+    }
+    else{
+        printf("o numero deve ser positivo!");
+    }*/
+
+
+
+    /*int cont = 1, soma = 0, num;
+    while(cont <= 5){
+        printf("informe o %d numero:", cont);
+        scanf("%d", &num);
+        soma += num;
+        cont++;
+    }
+       printf("soma dos numeros: %d", soma);
+
+
+    return 0;*/
+
+
+    /*int cont = 1, soma = 0, media, num;
+    while (cont <= 10){
+        printf("informe o %d numero:", cont);
+        scanf("%d", &num);
+        soma= soma + num;
+        cont++;
+    }
+    media = soma / 10;
+    printf("a media dos numeros eh: %d", media);*/
+
+
+
+        /* CORRIGIR:::
+        int num, soma = 0, cont = 0;
+        printf("informe um numero inteiro:");
+        scanf("%d", &num);
+        if (num <= 0){
+            while( num >= 0){
+            soma = num + cont;
+            num ++;
+            }
+            printf("a soma de todos os numeros eh %d", soma);
+
+        }*/
+
+
+
+
+
+    /*int cont = 1, n, num, somap= 0, somai =0, qnti= 0;
+    printf("informe a quantidade de numeros:");
+    scanf("%d", &n);
+    while(cont <= n){
+        printf("informe o %d numero:", cont);
+    scanf("%d", &num);
+    if(num %2 == 0){
+        somap += num;
+    }
+    else{
+        somai += num;
+        qnti++;
+    }
+        cont ++;
+    }
+printf("soma dos pares: %d\n", somap);
+printf("media dos impares: %d\n", (somai/qnti));
+    return 0;*/
+
+
+    /*int num, soma = 0, qnt = 0;
+    printf("informe um valor:");
+    scanf("%d", &num);
+    while(num != 0){
+    soma += num;
+    qnt++;
+    printf("informe um valor:");
+    scanf("%d", &num);
+    }
+    printf("media: %d\n",(soma/qnt));*/
+
+
+
+
+    /* DO WHILE */
+    /*int i= 1;
+    do{
+        printf("%d\t", i);
+        i++;
+    }
+    while(i <=10);*/
+        
+        
+        
+}
+
+
