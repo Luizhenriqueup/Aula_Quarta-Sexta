5e6fa53c9937feef66ee7d3d4a7b7a0bc5cd7d4b
Luizhenriqueup <luiz.up.work@gmail.com>
2025-09-05 21:43:23 -0300

    main.c

---
+++ aula_0509/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int num, cont = 1;
+    printf("informe um numero:");
+    scanf("%d", &num);
+    do{
+        printf("%d x %d = %d\n", num, cont, (num*cont));
+        cont++;
+    }
+        while(cont <=10);*/
+
+
+    /*int num, maior, menor, indice = 1;
+    do{
+        printf("informe um numero:");
+        scanf("%d", &num);
+        if (indice==1){
+            maior = num;
+            menor = num;
+            indice++;
+        }
+        else if (num != 0){
+            if(num > maior){
+                maior = num;
+            }
+            else if(num < menor){
+                menor = num;
+            }
+        }
+    }while (num !=0);
+    printf("maior:%d\n", maior);
+    printf("menor %d\n", menor);*/
+
+
+
+
+    /*for(int i =1; i <=10; i++){
+        printf("%d\t", i);
+    }*/
+
+
+    /*for(int i= 1; i <=20; i++){
+    printf("o valor quadrado dos numeros %d eh %d\t", i, (i * i));
+    }*/
+
+
+    /*int n1;
+    printf("Informe um numero:");
+    scanf("%d", &n1);
+    for(int i=0; i<=n1; i++){
+        if(i%3 == 0){
+            printf("%d\n", i);
+        }
+        else{
+
+        }
+    }*/
+
+
+    /*int n1;
+    printf("informe um numero:");
+    scanf("%d", &n1);
+    for(int i=n1; i>=0; i--){
+        printf("o numero de tras para frente informado: %d", i);
+    }*/
+
+    /*int soma=0;
+    for(int i = 100; i<=200; i ++){
+       if (i % 7 == 0){
+       soma += i;
+       }
+    }
+    printf("%d\t", soma);*/
+        return 0;
+}
